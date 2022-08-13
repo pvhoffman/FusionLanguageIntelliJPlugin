1cab74d9c03146a453a49bfa081e0e242ccf8088
GitHub Action <action@github.com>
2022-08-13 17:48:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/pvhoffman/fusionlanguageintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/pvhoffman/fusionlanguageintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pvhoffman.fusionlanguageintellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pvhoffman.fusionlanguageintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/pvhoffman/fusionlanguageintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pvhoffman/fusionlanguageintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pvhoffman.fusionlanguageintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pvhoffman.fusionlanguageintellijplugin.MyBundle
 
 class MyApplicationService {
 
